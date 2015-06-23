6c83e3d61e8dc496f69fdcddc671d69db94beb66
Mbit555 <viartsnet@gmail.com>
2015-06-23 22:22:39 +0300

    Add Elements on Form

--- Calculator/Calculator/MyForm.h
+++ Calculator/Calculator/MyForm.h
@@ -34,6 +34,17 @@ namespace Calculator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
 
 	private:
 		/// <summary>
@@ -48,16 +59,121 @@ namespace Calculator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(32, 29);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(48, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Число 1";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(32, 62);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(48, 13);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"Число 2";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(32, 96);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(37, 13);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Ответ";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(126, 29);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(126, 55);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(126, 89);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(24, 147);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(31, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"+";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(77, 147);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(31, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"-";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(139, 147);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(31, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"/";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(195, 147);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(31, 23);
+			this->button4->TabIndex = 2;
+			this->button4->Text = L"*";
+			this->button4->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Name = L"MyForm";
 			this->Text = L"Calculator";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
